3ce8e955a4f9bec5d8d2ee5bd6c1292b93302d18
Egor Kononenko <egorychmaster@gmail.com>
2020-06-30 22:34:48 +0300

    Add project FileRepositirySimple

---
+++ FileRepositirySimple/Controllers/FilesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileRepositirySimple.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get(string filename)
+        {
+
+
+            return Ok();
+        }
+    }
+}
